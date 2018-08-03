110c223a0fe3b7b1e5d34c3e02c67a739c9c4061
Vaibhav Singhal <33128077+TheVaibhavSinghal@users.noreply.github.com>
2018-08-03 16:53:15 +0530

    Add files via upload

--- p6.c
+++ p6.c
@@ -1,63 +1,94 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-/*
-for(i=0;i<n;i++)
- for(j=0;j<n-i-1;j++)
-  if(a[j]>a[j+1])
-    swap(a[j],a[j+1])
 
-
-
-*/
-
-void bSort(int a[100],int n)
+int bSort(int a[100],int n)
 {
- int i,j,temp;
- for(i=0;i<n;i++)
+ int i,j,temp,nocB=0;
+ for(i=1;i<n;i++)
  {
-   for(j=0;j<n-i-1;j++)
+  
+   for(j=0;j<n-i;j++)
    {
+     nocB++;
      if(a[j]>a[j+1])
      {
-	temp=a[j];
+	    temp=a[j];
         a[j]=a[j+1];
         a[j+1]=temp;
+        
      }
    }
- }
-
+    }
+return nocB;
 }
 
 
-void sSort(int a[100],int n)
+int sSort(int a[100],int n)
 {
- int i,j,max,imax,temp;
- for(i=0;i<n;i++)
+ int i,j,max,imax,temp,nocS=0;
+ for(i=1;i<n;i++)
  {
    max=a[0];
    imax=0;
-   for(j=1;j<n-i;j++)
+   for(j=1;j<=n-i;j++)
+   {
+     nocS++;
      if(a[j]>max) 
        {
          max=a[j];
-	 imax=j;
+	     imax=j;
        }
+   
+   }
    temp=a[n-i];
    a[n-i]=a[imax];
    a[imax]=temp;
  } 
+ return nocS;
 }
+
+
+int iSort(int a[100],int n)
+{
+ int i,j,temp,nocI=0;
+ for(i=1;i<n;i++)
+  {
+    for(j=i;j>0;j--)
+    {
+     nocI++;
+     if(a[j]<a[j-1])
+     {
+       temp=a[j];
+       a[j]=a[j-1];
+       a[j-1]=temp;
+     }
+     else break;
+    }
+   }
+   return nocI;    
+}
+
+  
+  
+  
+  
+
+
+
+
+
+
 void main()
 {
- int n[10]={10,20,30,40,50,60,70,80,90,100},i,j;
+ int n[10]={10,20,30,40,50,60,70,80,90,100},i,j,nocB,nocS,nocI;
  int a[10][100];
 
  for(i=0;i<10;i++)
  {
   for(j=0;j<n[i];j++)
     {
-    	a[i][j]=rand()%10;
+    	a[i][j]=rand()%100;
     }
  } 
  
@@ -74,7 +105,7 @@ void main()
  printf("the sorted array is:\n");
  for(i=0;i<10;i++)
   {
-    sSort(a[i],n[i]);
+    bSort(a[i],n[i]);
     for(j=0;j<n[i];j++)
 		printf(" %d",a[i][j]);
     printf("\n");
@@ -82,26 +113,14 @@ void main()
 
 
 
- /*printf("number of inputs\t\tnumber of comparison for search an item\n");
- for(i=0;i<20;i++)
- {
-   for(j=0;j<n[i];j++)
-     if(a[i][j]==num[i])
- 	   break;
+ printf("number of inputs\tbSort\tsSort\tiSort\n");
  
-  printf("%d\t\t\t%d\n",n[i],j+1);
-  wAvgNum += n[i]*(j+1);
+ for(i=0;i<10;i++)
+ {
+   nocB=bSort(a[i],n[i]);
+   nocS=sSort(a[i],n[i]);
+   nocI=iSort(a[i],n[i]);
+   printf("\t%d\t\t%d\t%d\t%d\n",n[i],nocB,nocS,nocI);
  } 
-
- wAvg=wAvgNum/wAvgDen;
-
- printf("\n\nWeighted Average is %f",wAvg);
-
-*/
-
-
-
-
-
 
 }
